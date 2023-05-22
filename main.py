37a39349d6e8ccfdf7c960f10bfcfc985f0ed49f
Nater0214 <gamer.nater0214@gmail.com>
2023-05-21 22:54:12 -0400

    Add argparsing

--- main.py
+++ main.py
@@ -1,3 +1,23 @@
 # main.py
 # Pip counter
 # Count pips on dice or dominoes
+
+
+# Imports
+import argparse
+
+
+# Definitions
+def main(file_path: str) -> int:
+    """Main"""
+    
+    print(file_path)
+
+
+# Run
+if __name__ == "__main__":
+    
+    arg_parser = argparse.ArgumentParser(prog="Pip Counter",add_help=True)
+    arg_parser.add_argument("-i", type=str, help="Input file", required=True)
+    args = arg_parser.parse_args()
+    main(args.i)
